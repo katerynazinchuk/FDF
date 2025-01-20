ea1132e57f0d8f3a79df6853e4ef0278a0244d33
Kateryna Zinchuk <kzinchuk@f0r5s7.codam.nl>
2025-01-20 16:11:21 +0100

    Initial commit

---
+++ include/fdf.h
@@ -0,0 +1,92 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzinchuk <kzinchuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 18:22:54 by kzinchuk          #+#    #+#             */
+/*   Updated: 2025/01/18 20:28:13 by kzinchuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <MLX42/MLX42_Int.h>
+# include "MLX42/MLX42.h"
+# include <math.h>
+# include <fcntl.h>
+# include "get_next_line.h"
+# include "libft.h"
+
+# define WIDTH 2000
+# define HEIGHT 1000
+// # define ZOOM_STEP 1.1
+// #define ROT_STEP M_PI / 24
+
+typedef struct s_node
+{
+    char            *line;
+    struct s_node   *next;
+} t_node;
+
+typedef struct s_point {
+    float   x;
+    float   y;
+    float   z;
+    uint32_t color;
+}           t_point;
+
+typedef struct s_map {
+    t_point     **points;
+    int32_t     width;
+    int32_t     height;
+    int32_t     z_min;
+    int32_t     z_max;
+    int32_t     color_range;
+}               t_map;
+
+
+typedef struct s_line
+{
+    int         dx;
+    int         dy;
+    int         sx;
+    int         sy;
+    int         err;
+    int         e2;
+}               t_line;
+
+typedef struct s_fdf
+{
+    void        *win;
+    mlx_t       *mlx;
+    mlx_image_t *img;
+    t_map       *map;
+}               t_fdf;
+
+typedef struct s_iterator {
+    int i;
+    int j;
+}       t_iterator;
+
+t_node *add_node(char *line);
+t_node *create_list(t_node **head, char *line);
+void free_list(t_node *head);
+t_node *parse_fdf_map(const char *filename);
+size_t count_strings(const char *s, char delimiter);
+int validate_map(t_node *map_lines, char delimiter);
+//static int parse_line_to_points(const char *line, t_point *points, size_t y, size_t width);
+//static t_point **allocate_2d_array(size_t height, size_t width);
+t_map *initialize_map(t_node *map_lines);
+t_map *populate_map(t_map *map, t_node *map_lines);
+t_map *convert_to_map(t_node *map_lines);
+
+
+
+
+#endif
---
+++ src/color.c
@@ -0,0 +1,90 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   color.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzinchuk <kzinchuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 19:09:43 by kzinchuk          #+#    #+#             */
+/*   Updated: 2025/01/13 13:05:01 by kzinchuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+// int get_rgba(int r, int g, int b, int a)
+// {
+//     return (r << 24 | g << 16 | b << 8 | a);
+// } 
+// int interpolate_color(int start, int end, float progress)
+// {
+//     int r = (int)(((1 - progress) * ((start >> 16) & 0xFF)) + (progress * ((end >> 16) & 0xFF)));
+//     int g = (int)(((1 - progress) * ((start >> 8) & 0xFF)) + (progress * ((end >> 8) & 0xFF)));
+//     int b = (int)(((1 - progress) * (start & 0xFF)) + (progress * (end & 0xFF)));
+
+//     return (r << 16 | g << 8 | b);
+// }
+// void scale_points(t_map *map, float scale_factor)
+// {
+//     for (int y = 0; y < map->height; y++)
+//     {
+//         for (int x = 0; x < map->width; x++)
+//         {
+//             map->points[y][x].x *= scale_factor;
+//             map->points[y][x].y *= scale_factor;
+//             map->points[y][x].z *= scale_factor;
+//         }
+//     }
+// }
+
+// void rotate_x(t_point *point, double alpha)
+// {
+//     float temp_y = point->y;
+//     float temp_z = point->z;
+//     point->y = temp_y * cos(alpha) - temp_z * sin(alpha);
+//     point->z = temp_y * sin(alpha) + temp_z * cos(alpha);
+// }
+
+// void rotate_y(t_point *point, double beta)
+// {
+//     float temp_x = point->x;
+//     float temp_z = point->z;
+//     point->x = temp_x * cos(beta) + temp_z * sin(beta);
+//     point->z = -temp_x * sin(beta) + temp_z * cos(beta);
+// }
+
+// void rotate_z(t_point *point, double gamma)
+// {
+//     float temp_x = point->x;
+//     float temp_y = point->y;
+//     point->x = temp_x * cos(gamma) - temp_y * sin(gamma);
+//     point->y = temp_x * sin(gamma) + temp_y * cos(gamma);
+// }
+// void translate_points(t_map *map, float x_offset, float y_offset)
+// {
+//     for (int y = 0; y < map->height; y++)
+//     {
+//         for (int x = 0; x < map->width; x++)
+//         {
+//             map->points[y][x].x += x_offset;
+//             map->points[y][x].y += y_offset;
+//         }
+//     }
+// }
+
+// void transform_point(t_point *point, t_camera *camera)
+// {
+//     // Apply rotations
+//     rotate_x(point, camera->alpha);
+//     rotate_y(point, camera->beta);
+//     rotate_z(point, camera->gamma);
+
+//     // Apply zoom
+//     point->x *= camera->zoom;
+//     point->y *= camera->zoom;
+//     point->z /= camera->z_divisor; // Scale z-axis separately
+
+//     // Apply offsets
+//     point->x += camera->x_offset;
+//     point->y += camera->y_offset;
+// }
